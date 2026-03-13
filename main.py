b90d0cba80e97d8bdd310f650c2762acacc578ec
invvme <larisamaksimova2009@gmail.com>
2026-03-13 16:59:01 +0300

    git1_3

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import sys
 import random
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
 
-class MainWindow(QtWidgets.QMainWindow):
+
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    def setupUi(self):
+        self.setWindowTitle("Random Circles")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.centralwidget)
+
+        self.pushButton = QtWidgets.QPushButton("нажми меня", self.centralwidget)
+        self.pushButton.setGeometry(280, 240, 191, 41)
+
+
+class MainWindow(Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("qt.ui", self)
+        self.setupUi()
         self.pushButton.clicked.connect(self.draw_circle)
         self.circles = []
 
@@ -14,14 +27,17 @@ class MainWindow(QtWidgets.QMainWindow):
         diameter = random.randint(20, 100)
         x = random.randint(0, self.centralwidget.width())
         y = random.randint(0, self.centralwidget.height())
-        self.circles.append((x, y, diameter))
+
+        color = QtGui.QColor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
-        painter.setBrush(QtGui.QBrush(QtGui.QColor("yellow")))
         painter.setPen(QtCore.Qt.NoPen)
-        for x, y, diameter in self.circles:
+        for x, y, diameter, color in self.circles:
+            painter.setBrush(QtGui.QBrush(color))
             painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == "__main__":
